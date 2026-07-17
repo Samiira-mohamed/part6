4583b3bb1c1e268111fbb65e72185eb5dcbef99b
Student <student@sirafrica.org>
2026-07-17 12:08:10 +0300

    6.17: add new anecdotes with TanStack Query

--- query-anecdotes/src/App.jsx
+++ query-anecdotes/src/App.jsx
@@ -1,12 +1,29 @@
-import { useQuery } from '@tanstack/react-query'
-import { getAnecdotes } from './requests'
+import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query'
+import { getAnecdotes, createAnecdote } from './requests'
 
 const App = () => {
+  const queryClient = useQueryClient()
+
   const result = useQuery({
     queryKey: ['anecdotes'],
     queryFn: getAnecdotes,
     retry: 1
   })
+
+  const newAnecdoteMutation = useMutation({
+    mutationFn: createAnecdote,
+    onSuccess: (newAnecdote) => {
+      const anecdotes = queryClient.getQueryData(['anecdotes'])
+      queryClient.setQueryData(['anecdotes'], anecdotes.concat(newAnecdote))
+    }
+  })
+
+  const addAnecdote = (event) => {
+    event.preventDefault()
+    const content = event.target.anecdote.value
+    event.target.reset()
+    newAnecdoteMutation.mutate({ content, votes: 0 })
+  }
 
   if (result.isPending) {
     return <div>loading data...</div>
@@ -21,6 +38,12 @@ const App = () => {
   return (
     <div>
       <h3>Anecdote app</h3>
+
+      <form onSubmit={addAnecdote}>
+        <input name="anecdote" />
+        <button type="submit">create</button>
+      </form>
+
       {anecdotes.map(anecdote => (
         <div key={anecdote.id}>
           <div>{anecdote.content}</div>
