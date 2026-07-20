0d894bc389a71b3e1ba822bd913e6436d6b2beb3
Student <student@sirafrica.org>
2026-07-20 10:10:31 +0300

    6.2: unicafe UI with Redux

--- unicafe-redux/src/main.jsx
+++ unicafe-redux/src/main.jsx
@@ -1,9 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+
 import App from './App.jsx'
+import counterReducer from './reducers/reducer'
+
+const store = createStore(counterReducer)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </StrictMode>,
 )
